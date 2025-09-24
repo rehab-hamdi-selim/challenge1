605b4b7e08939d9b02acbefbcfadcd3d07287657
Rehab Hamdy <209379697+rehab-hamdi-selim@users.noreply.github.com>
2025-09-24 09:20:20 +0300

    task model

---
+++ lib/features/home/data/models/task_model.dart
@@ -0,0 +1,15 @@
+class Task {
+  final String id;
+  final String title;
+  final bool isDone;
+
+  const Task({required this.id, required this.title, required this.isDone});
+
+  Task copyWith({String? id, String? title, bool? isDone}) {
+    return Task(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      isDone: isDone ?? this.isDone,
+    );
+  }
+}
